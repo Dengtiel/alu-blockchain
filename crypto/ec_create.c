b50394caaaddb1596ee463a2f52fe49ed94686fd
Dengtiel <d.akol@alustudent.com>
2026-09-09 11:49:31 +0200

    Add ec_create function to generate EC key pairs

---
+++ crypto/ec_create.c
@@ -0,0 +1,24 @@
+#include "hblk_crypto.h"
+
+/**
+ * ec_create - Creates a new EC key pair using the secp256k1 curve
+ *
+ * Return: Pointer to an EC_KEY structure containing both the
+ *         public and private keys, or NULL upon failure
+ */
+EC_KEY *ec_create(void)
+{
+	EC_KEY *key;
+
+	key = EC_KEY_new_by_curve_name(EC_CURVE);
+	if (key == NULL)
+		return (NULL);
+
+	if (EC_KEY_generate_key(key) != 1)
+	{
+		EC_KEY_free(key);
+		return (NULL);
+	}
+
+	return (key);
+}
--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -1,9 +1,17 @@
 #ifndef HBLK_CRYPTO_H
 #define HBLK_CRYPTO_H
 
+#define OPENSSL_API_COMPAT 0x10100000L
+#define OPENSSL_NO_DEPRECATED
+
 #include <stdint.h>
 #include <stddef.h>
 #include <openssl/sha.h>
+#include <openssl/ec.h>
+#include <openssl/obj_mac.h>
+
+/* Elliptic curve used for key generation */
+#define EC_CURVE NID_secp256k1
 
 /**
  * sha256 - Computes the SHA256 hash of a sequence of bytes
@@ -15,5 +23,13 @@
  */
 uint8_t *sha256(int8_t const *s, size_t len,
 	uint8_t digest[SHA256_DIGEST_LENGTH]);
+
+/**
+ * ec_create - Creates a new EC key pair
+ *
+ * Return: Pointer to an EC_KEY structure containing both the
+ *         public and private keys, or NULL upon failure
+ */
+EC_KEY *ec_create(void);
 
 #endif /* HBLK_CRYPTO_H */
